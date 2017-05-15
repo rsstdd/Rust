c5cf6db23528ef4649a33cf250d2cdf9c238c1c0
rsstdd <rosstod@gmail.com>
2017-05-14 21:03:13 -0700

    Guessing game complete

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,6 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("The secret number is: {}", secret_number);
-
     loop {
         println!("Please input your guess.");
 
@@ -19,15 +17,20 @@ fn main() {
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse()
-            .expect("Failed to read line");
+        let guess: u32 = guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
             Ordering::Less    => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal   => println!("You win!"),
+            Ordering::Equal   => {
+                println!("You win!");
+                break;
+            }
         }
     }
 }
